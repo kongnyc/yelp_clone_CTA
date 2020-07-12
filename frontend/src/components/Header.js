dcbe50d59ed463a101709249e5f2421944011ee3
kongnyc <bbsyoow@gmail.com>
2020-07-12 10:10:02 -0400

    update header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,10 +20,10 @@ const [userLogin, setLogin] = useState(sessionStorage.userLogin)
 
     } else {
         return (
-            <ul className="box">
+            <ul className="linkBtn">
                   <li className="login"><a href="/login/">Log In</a></li>
-                  <li className="login"><a href="/signup/">User Sign Up</a></li>
-                  <li className="businessLogin"><a href="/business/">Business Sign Up</a></li>
+                  <li className="login"><a href="/signup/">Sign Up</a></li>
+                  <li className="businessLogin"><a href="/business/">Store Sign Up</a></li>
             </ul> 
         )
     }
@@ -41,7 +41,8 @@ const [userLogin, setLogin] = useState(sessionStorage.userLogin)
 
         return (
             <div className="header">
-            <img src="https://images.squarespace-cdn.com/content/v1/5b2424ad266c07b94eab58ee/1536675878312-M9F797DGCA2DWEIOL248/ke17ZwdGBToddI8pDm48kHyVHa5xa_3FTr0ohEyBrNVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7TNdqww6z_61iXx7yO_mnkqr9QvTuVKJiB1R_XE5p9s9p_nV8m35JPLFe4dXvYPk4A/yelp+logo.png" alt="logo"></img>
+            
+            <img className="logo" src="https://images.squarespace-cdn.com/content/v1/5b2424ad266c07b94eab58ee/1536675878312-M9F797DGCA2DWEIOL248/ke17ZwdGBToddI8pDm48kHyVHa5xa_3FTr0ohEyBrNVZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7TNdqww6z_61iXx7yO_mnkqr9QvTuVKJiB1R_XE5p9s9p_nV8m35JPLFe4dXvYPk4A/yelp+logo.png" alt="logo"></img>
             {loginCheck()}
             </div>
         )
